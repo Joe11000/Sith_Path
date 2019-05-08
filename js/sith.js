d3b60a519a4cfb54872e40ff3722722e0b729f63
Joe Noonan <joenoonan27@gmail.com>
2019-05-08 16:59:19 -0500

    why wont this line up

---
+++ js/sith.js
@@ -0,0 +1,6 @@
+// function sith_text_style() {
+//   debugger
+//   var sith_text = document.getElementById('sith_text');
+//   sith_text.style.WebkitBackgroundClip = 'text';
+//   sith_text.style.WebkitTextFillColor = 'transparent'
+// }
--- sith.js
+++
@@ -1,5 +0,0 @@
-function sith_text_style() {
-  var sith_text = document.getElementById('sith_text');
-  sith_text.style.WebkitBackgroundClip = 'text';
-  sith_text.style.WebkitTextFillColor = 'transparent'
-}
