fe0767cc110393d905ee160e2df63f854e5b01b2
Joe Noonan <joenoonan27@gmail.com>
2019-05-08 14:18:23 -0500

    transfer dashes to underscores

---
+++ sith.js
@@ -0,0 +1,5 @@
+function sith_text_style() {
+  var sith_text = document.getElementById('sith_text');
+  sith_text.style.WebkitBackgroundClip = 'text';
+  sith_text.style.WebkitTextFillColor = 'transparent'
+}
